c87bb402ef9e562a80ff088eea76b30933b77b1f
WhyILived <85578537+WhyILived@users.noreply.github.com>
2024-11-19 19:52:55 -0500

    Update 198_MPU6050.c

--- 198_MPU6050.c
+++ 198_MPU6050.c
@@ -33,49 +33,49 @@ void I2C1_Init(void) {
 
 
     GPIOB->MODER |= (2U << GPIO_MODER_MODER8_Pos) | (2U << GPIO_MODER_MODER9_Pos);
-    GPIOB->OTYPER |= GPIO_OTYPER_OT_8 | GPIO_OTYPER_OT_9;  // Open-drain
-    GPIOB->OSPEEDR |= (3U << GPIO_OSPEEDER_OSPEEDR8_Pos) | (3U << GPIO_OSPEEDER_OSPEEDR9_Pos);  // High speed
-    GPIOB->PUPDR |= (1U << GPIO_PUPDR_PUPDR8_Pos) | (1U << GPIO_PUPDR_PUPDR9_Pos);  // Pull-up
-    GPIOB->AFR[1] |= (4U << GPIO_AFRH_AFSEL8_Pos) | (4U << GPIO_AFRH_AFSEL9_Pos);  // AF4 for I2C1
+    GPIOB->OTYPER |= GPIO_OTYPER_OT_8 | GPIO_OTYPER_OT_9; 
+    GPIOB->OSPEEDR |= (3U << GPIO_OSPEEDER_OSPEEDR8_Pos) | (3U << GPIO_OSPEEDER_OSPEEDR9_Pos);
+    GPIOB->PUPDR |= (1U << GPIO_PUPDR_PUPDR8_Pos) | (1U << GPIO_PUPDR_PUPDR9_Pos);
+    GPIOB->AFR[1] |= (4U << GPIO_AFRH_AFSEL8_Pos) | (4U << GPIO_AFRH_AFSEL9_Pos);
 
-    I2C1->CR1 = 0;  // Disable I2C1 for configuration
-    I2C1->CR2 = 16; // APB1 clock frequency in MHz
-    I2C1->CCR = 80; // Standard mode (100kHz) clock
-    I2C1->TRISE = 17;  // Max rise time
-    I2C1->CR1 |= I2C_CR1_PE;  // Enable I2C1
+    I2C1->CR1 = 0;
+    I2C1->CR2 = 16; 
+    I2C1->CCR = 80; 
+    I2C1->TRISE = 17;
+    I2C1->CR1 |= I2C_CR1_PE;
 }
 
 void I2C_Start(void) {
-    I2C1->CR1 |= I2C_CR1_START;  // Generate START condition
-    while (!(I2C1->SR1 & I2C_SR1_SB));  // Wait for START condition to be generated
+    I2C1->CR1 |= I2C_CR1_START;
+    while (!(I2C1->SR1 & I2C_SR1_SB));
 }
 
 void I2C_Write(uint8_t data) {
-    I2C1->DR = data;  // Write data to data register
-    while (!(I2C1->SR1 & I2C_SR1_TXE));  // Wait for data transmission
+    I2C1->DR = data;
+    while (!(I2C1->SR1 & I2C_SR1_TXE));
 }
 
 uint8_t I2C_Read_Ack(void) {
-    I2C1->CR1 |= I2C_CR1_ACK;  // Enable ACK
-    while (!(I2C1->SR1 & I2C_SR1_RXNE));  // Wait for data reception
+    I2C1->CR1 |= I2C_CR1_ACK;
+    while (!(I2C1->SR1 & I2C_SR1_RXNE));
     return I2C1->DR;
 }
 
 uint8_t I2C_Read_Nack(void) {
-    I2C1->CR1 &= ~I2C_CR1_ACK;  // Disable ACK
-    while (!(I2C1->SR1 & I2C_SR1_RXNE));  // Wait for data reception
+    I2C1->CR1 &= ~I2C_CR1_ACK; 
+    while (!(I2C1->SR1 & I2C_SR1_RXNE)); 
     return I2C1->DR;
 }
 
 void I2C_Stop(void) {
-    I2C1->CR1 |= I2C_CR1_STOP;  // Generate STOP condition
+    I2C1->CR1 |= I2C_CR1_STOP; 
 }
 
 void MPU6050_Init(void) {
     I2C_Start();
-    I2C_Write(MPU6050_ADDR << 1);  // Send slave address with write bit
-    I2C_Write(PWR_MGMT_1);  // Write to power management register
-    I2C_Write(0x00);  // Wake up MPU6050
+    I2C_Write(MPU6050_ADDR << 1);
+    I2C_Write(PWR_MGMT_1);
+    I2C_Write(0x00);
     I2C_Stop();
 }
 
